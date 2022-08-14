53ce0c41c1d7614c55cc8fe647304a63dfc1bfdf
vasanthavassu <110987790+vasanthavassu@users.noreply.github.com>
2022-08-14 08:24:11 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i=5,i,j;
+char ch1=’A’ ,ch2=’a’;
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+if(i>=j)
+{
+Printf(“%c%c  “,ch1,ch2);
+Ch1++;
+Ch2++;
+}
+else
+Printf(“ “);
+}
+Printf(“\n”);
+}
+Return 0;
+}
